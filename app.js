2e409e720d438e92fc606e92b36276a36ee9a3f9
hameema <48262172+hameema@users.noreply.github.com>
2021-05-12 16:14:33 +0200

    new upgrades
    - new download page
    - support for multiple download zipped
    -search files and folders

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ require("./mvc/config/passport");
 
 const uploadRouter = require("./mvc/routes/upload");
 const loginRouter = require("./mvc/routes/users");
+const downloadCtrl = require("./mvc/controllers/download");
+const uploadCtrl = require("./mvc/controllers/upload");
 const authCtrl = require("./mvc/controllers/authenticate");
 
 const app = express();
@@ -27,26 +29,41 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 
-app.use("/download", express.static(path.join(__dirname, "uploads")), serveIndex(path.join(__dirname, "uploads"), {"icons": true}));
+// app.use("/download", express.static(path.join(__dirname)), serveIndex(path.join(__dirname), {"icons": true,"view":"details"}));
 app.use("/", loginRouter);
 
-app.use(authCtrl.checkToken);
 app.use("/", uploadRouter);
+app.use(
+  "/download",
+  express.static("C://Users/HAMIM/Documents/CodeProjects/EEE-Upload-Server"),
+  downloadCtrl.serveIndex(
+    "C://Users/HAMIM/Documents/CodeProjects/EEE-Upload-Server",
+    {
+      icons: true,
+      views: "details",
+      template: (locals, res) => {
+        res.render("download", { locals: locals });
+      },
+    }
+  )
+);
+
+app.post("/downloads", downloadCtrl.downloadFiles);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
--- mvc/routes/upload.js
+++ mvc/routes/upload.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const uploadCtrl = require("../controllers/upload");
+const authCtrl = require("../controllers/authenticate");
 
 router
-  .get("/upload", uploadCtrl.renderUploadPage)
-  .get("/upload/:subjectID/:fileType", (req, res, next) => {
-    res.redirect("/upload");
-  })
-  .post(
+  .get("/upload", authCtrl.checkToken, uploadCtrl.renderUploadPage)
+  .get(
     "/upload/:subjectID/:fileType",
-    uploadCtrl.Upload
-  );
+    authCtrl.checkToken,
+    (req, res, next) => {
+      res.redirect("/upload");
+    }
+  )
+  .post("/upload/:subjectID/:fileType", authCtrl.checkToken, uploadCtrl.Upload);
 
 module.exports = router;
