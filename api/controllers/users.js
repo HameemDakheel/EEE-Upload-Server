1981132c5d538d0f80a889385e064978f131f28d
Hameem Dakheel <HameemDakheel@outlook.com>
2023-05-14 08:53:52 +0200

    fixing users api bugs

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
-const User = mongoose.model("User");
+const User = require("../modules/users");
 const jwt = require("jsonwebtoken");
-const logger = require("../config/logger")
+const logger = require("../config/logger");
+const Path = require("path");
+const defaultPath = process.env.DEFAULT_PATH;
+const trashPath = process.env.TRASH_PATH;
+const fs = require("fs");
+const formidable = require("formidable");
 
+//Done
 function getUsers(req, res) {
-  const message= "";
+  const message = "";
   User.find({}, (err, users) => {
-    var data= [];
+    var data = [];
     users.map((user) => {
       if (user.privileges === "admin") {
         return;
       } else {
-        data.push({ username: user.userName, path: user.defaultPath });
+        data.push({ username: user.username, path: user.defaultPath });
         return;
       }
     });
-    if (err || users.length<=0){
+    if (err || users.length <= 0) {
       return res.status(400).json({ error: "true", message: err });
     }
 
@@ -23,6 +29,7 @@ function getUsers(req, res) {
   });
 }
 
+//DONE
 function login(req, res) {
   const { username, password } = req.body;
   if (!username || !password) {
@@ -30,7 +37,7 @@ function login(req, res) {
       .status(401)
       .json({ error: "true", message: "all fields are required" });
   }
-  User.findOne({ userName: username }, function (err, user) {
+  User.findOne({ username: username }, function (err, user) {
     if (err || !user) {
       return res
         .status(401)
@@ -41,7 +48,7 @@ function login(req, res) {
         error: "",
         message: "login success",
         token: user.generateToken(),
-        username: user.userName,
+        username: user.username,
       });
     } else {
       return res.status(401).json({ error: "true", message: "wrong password" });
@@ -49,62 +56,86 @@ function login(req, res) {
   });
 }
 
+//DONE
 function register(req, res) {
   const { username, password, path } = req.body;
-  User.findOne({ userName: username }, function (err, user) {
-    if (err || user) {
-      return res.status(403).json({ error: "true", message:"user already exists" });
-    }
-  })
   if (!username || !password || !path)
     return res
       .status(401)
       .json({ error: "true", message: "all fields are required" });
-  try {
-    const user = new User();
-    user.userName = username;
-    user.setPassword(password);
-    user.defaultPath = path;
-    user.save();
-    logger.info(`${req.decode.username} add new user ${username}`);
-    res.status(200).json({ error: "", message: "user created" });
-  } catch (e) {
-    logger.error(e.message);
-    res.status(400).json({ error: "true", message: e.message });
+  var userAlreadyExsit = User.findOne(
+    { username: username },
+    function (err, user) {
+      console.log("hi", err, user);
+      if (err || user) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  );
+  if (userAlreadyExsit) {
+    return res
+      .status(403)
+      .json({ error: "true", message: "user already exists" });
+  } else {
+    try {
+      const user = new User();
+      user.username = username;
+      user.setPassword(password);
+      user.defaultPath = path || "/";
+      let realPath = Path.join(defaultPath, path);
+      if (!fs.existsSync(realPath)) {
+        fs.mkdirSync(realPath);
+      }
+      user.save();
+      logger.info(`${req.decoded.payload.username} add new user ${username}`);
+      res.status(200).json({ error: "", message: "user created" });
+    } catch (e) {
+      console.log(e.message);
+      logger.error(e.message);
+      res.status(400).json({ error: "true", message: e.message });
+    }
   }
 }
 
+//DONE
 function deleteUser(req, res) {
   if (!req.body.username) {
     return res
       .status(401)
       .json({ error: "true", message: "all fields are required" });
   }
-  User.findOneAndDelete({ userName: req.body.username }, (err, user) => {
+  User.deleteOne({ username: req.body.username }, (err, user) => {
     if (err) {
       return res.status(500).json({ error: "true", message: err });
     } else {
-      logger.warn(`${req.body.username} has been deleted by ${req.decoded.payload.username}`);
+      logger.warn(
+        `${req.body.username} has been deleted by ${req.decoded.payload.username}`
+      );
       return res.status(200).json({ error: "", message: "user deleted" });
     }
   });
 }
 
+//DONE
 function updateUser(req, res) {
-  const { username, path } = req.body;
-  if (!username || !path) {
+  const { oldUsername, username, oldPath, path } = req.body;
+  if (!username || !path || !oldUsername || !oldPath) {
     return res
-    .status(401)
-    .json({ error: "true", message: "all fields are required" });
+      .status(401)
+      .json({ error: "true", message: "all fields are required" });
   }
-  User.findOne({ userName: username }, (err, user) => {
+
+  User.findOne({ username: oldUsername }, (err, user) => {
     if (err || !user) {
+      log;
       return res
         .status(400)
         .json({ error: "true", message: err || "user not found" });
     } else {
       try {
-        user.userName = username;
+        user.username = username;
         user.defaultPath = path;
         user.save();
         logger.warn(
@@ -112,40 +143,50 @@ function updateUser(req, res) {
         );
         return res.status(200).json({ message: "user updated", user });
       } catch (e) {
-        logger.error(e)
+        logger.error(e);
         return res.status(500).json({ error: "true", message: e.message });
       }
     }
   });
 }
 
+//DONE
 function checkToken(req, res, next) {
   try {
-    let token = req.body.token? req.body.token : req.headers["authorization"].split(" ")[1];
+    let token = req.body.token
+      ? req.body.token
+      : req["headers"].authorization.split(" ")[1];
     var decode = jwt.verify(token, process.env.JWT_SECRET);
     var decoded = jwt.decode(token, { complete: true });
-    if (decode) {
+    if (decoded) {
       if (req.path === "/user/check-token") {
-        return res
-          .status(200)
-          .json({ error: "", message: "token is valid", decode:{username: decode.username, privileges:decode.privileges} });
+        return res.status(200).json({
+          error: "",
+          message: "token is valid",
+          decode: {
+            username: decode.username,
+            privileges: decode.privileges,
+          },
+        });
       } else {
         req.decoded = decoded;
         next();
       }
     }
   } catch (e) {
+    console.log(e.message);
     return res.status(401).json({ error: "true", message: "token is invalid" });
   }
 }
 
+//DONE
 function checkPrivileges(req, res, next) {
   const user = req.decoded.payload;
-  if (user.privileges === "admin"){
-      next();
-    } else {
-      return res.status(403).json({ error: "true", message: "Forbidden" });
-    }
+  if (user.privileges === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ error: "true", message: "Forbidden" });
+  }
 }
 
 module.exports = {
--- api/modules/users.js
+++ api/modules/users.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 const User = new mongoose.Schema({
-  userName: {
+  username: {
     type: String,
     unique: true,
     required: true,
   },
-  password:{
-    type:String,
-    required:true
+  password: {
+    type: String,
+    required: true,
   },
   salt: {
     type: String,
@@ -24,34 +24,39 @@ const User = new mongoose.Schema({
   },
   privileges: {
     type: String,
-    default: "sub-user",
-	unique: true,
-},
-defaultPath:{
-	type:String,
-	required:true,
-}
+    default: "none",
+  },
+  defaultPath: {
+    type: String,
+    required: true,
+  },
 });
 
-User.methods.setPassword = function (password){
+User.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(32).toString("hex");
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
-  this.password= password;
-
-}
-
-User.methods.checkPassword = function (password) {
-  // let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
-
-  // return this.hash === hash;
-  return this.password === password;
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+  this.password = password;
 };
 
-User.methods.generateToken = function (){
-  let token = jwt.sign({id: this._id ,username:this.userName, privileges:this.privileges}, process.env.JWT_SECRET, {
-    expiresIn: '24h'
-  });
-  return token
-}
+User.methods.checkPassword = function (password) {
+  let hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
 
-mongoose.model('User', User);
+  return this.hash === hash || this.password === password;
+};
+
+User.methods.generateToken = function () {
+  let token = jwt.sign(
+    { id: this._id, username: this.username, privileges: this.privileges },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+  return token;
+};
+
+module.exports = mongoose.model("User", User);
