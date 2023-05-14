0803702f39ff0325664de47f2fb2ab2a6d31082e
Hameem Dakheel <HameemDakheel@outlook.com>
2023-05-14 08:52:24 +0200

    fixing upload api bugs

--- api/controllers/upload.js
+++ api/controllers/upload.js
@@ -3,12 +3,13 @@ const User = mongoose.model("User");
 const formidable = require("formidable");
 const path = require("path");
 const fs = require("fs");
-const logger = require('../config/logger');
+const logger = require("../config/logger");
+const util = require("util");
 
 function Upload(req, res) {
   let username = req.decoded.payload.username;
-  let pathname = req.query.path|| "/";
-  let upload_path = path.join(process.env.DEFAULT_PATH,username,pathname);
+  let pathname = req.query.path || "/";
+  let upload_path = path.join(process.env.DEFAULT_PATH, username, pathname);
   let message;
   try {
     if (!fs.existsSync(upload_path)) {
@@ -20,32 +21,38 @@ function Upload(req, res) {
     logger.error(error);
   }
 
-  const form = new formidable({
+  const form = new formidable.IncomingForm({
     multiples: true,
     uploadDir: upload_path,
-    maxFileSize: 2 * 1024 * 1024 * 1024,
+    maxFileSize: 3 * 1024 * 1024 * 1024,
     keepExtension: true,
   });
 
+  const files = [];
+  const fields = [];
+
   form
     .on("fileBegin", (name, file) => {
-      file.path = path.join(upload_path, file.name);
+      file.filepath = path.join(form.uploadDir, name);
     })
-    .on("file",(fileName, file) => {
-      logger.info(`${username} is uploading ${fileName} to ${file.path}`);
+    .on("file", (fileName, file) => {
+      files.push({ fileName, path: file.filepath });
+      logger.info(`${username} is uploading ${fileName} to ${file.filepath}`);
+    })
+    .on("field", (field, value) => {
+      fields.push({ field, value });
     })
     .on("end", () => {
-      logger.info(`${username} is finish uploading`)
-      message = "Upload Complete";
+      logger.info(`${username} is finish uploading`);
+      message = `received files:\n\n${util.inspect(files)}`;
+      res.status(200).send(message);
     })
-	.on("error", (error) => {
-		logger.error(error);
-	})
+    .on("error", (error) => {
+      console.log(error);
+      logger.error(error);
+    });
   form.parse(req);
-  res.status(200).send(message);
 }
-
-
 
 module.exports = {
   Upload,
